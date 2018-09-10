8bda55a26afac12e1b2f9f5dd151ad9cb85670f0
agungatidhira@Nyawaku <agungatidhira@gmail.com>
2018-09-10 18:06:53 +0700

    ubah method jadi aggregation

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 "use strict"
 class Animal {
-  constructor(classification, num_legs, food_type) {
+  constructor(classification, num_legs, food_type, superPower) {
     this.classification = classification
     this.numLegs = num_legs
     this.foodType = food_type
+    this.superPower = superPower
   }
 
 }
 
 class Lion extends Animal{
-  constructor (classification, num_legs, food_type, habitate) {
-    super(classification, num_legs, food_type)
+  constructor (classification, num_legs, food_type, habitate, superPower) {
+    super(classification, num_legs, food_type, superPower)
     this.habitate = habitate
-    this.superPower = new SuperPower()
   }
 }
 
 class Eagle extends Animal{
-  constructor (classification, num_legs, food_type, has_wings) {
-    super(classification, num_legs, food_type)
+  constructor (classification, num_legs, food_type, has_wings, superPower) {
+    super(classification, num_legs, food_type, superPower)
     this.hasWings = has_wings
-    this.superPower = new SuperPower()
   }
 }
 
 class Shark extends Animal{
-  constructor (classification, num_legs, food_type, speed) {
-    super(classification, num_legs, food_type)
+  constructor (classification, num_legs, food_type, speed, superPower) {
+    super(classification, num_legs, food_type, superPower)
     this.speed = speed
-    this.superPower = new SuperPower()
   }
 }
 
@@ -46,13 +44,16 @@ class SuperPower {
     
   }
 }
+const superPower = new SuperPower()
 
-const lion = new Lion('mammal', 4, 'carnivore', 'africa')
-const eagle = new Eagle('aves', 2, 'carnivore', true)
-const shark = new Shark('fish', 0, 'carnivore', '75 Km/h')
+const lion = new Lion('mammal', 4, 'carnivore', 'africa', superPower)
+const eagle = new Eagle('aves', 2, 'carnivore', true, superPower)
+const shark = new Shark('fish', 0, 'carnivore', '75 Km/h', superPower)
 console.log(lion);
 console.log(eagle);
 console.log(shark);
+
+
 
 lion.superPower.superStrength();
 eagle.superPower.superIQ();
