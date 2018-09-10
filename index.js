d09e6a5c0ab6293921dd9c2efdbaa674460541bf
Rudy <peace1spirit@gmail.com>
2018-09-10 20:11:44 +0700

    tugas

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 "use strict"
+
+class Animal{
+    constructor(name,num_legs){
+        this.name=name;
+        this.num_legs=num_legs;
+    }
+}
+class Frog extends Animal{
+    constructor(name,num_legs,color){
+        super(name,num_legs);
+        this.color=color;
+        this.superpower=new Superpower(true,false);
+    }
+    is_warm_blooded(){
+        return true;
+    }
+}
+
+class Superpower{
+    constructor(userlaser,invisible){
+        this.uselaser=userlaser;
+        this.invisible=invisible;
+    }
+    use_laser_vision(){
+        return this.uselaser;
+    }
+    be_invisible(){
+        return this.invisible;
+    }
+}
+var flog1= new Frog("frog",4,"blue");
+console.log(flog1);
+console.log(flog1.superpower.use_laser_vision());
