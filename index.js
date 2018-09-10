183f05ef1c73fa9ee1bd7e02affed42c4844da71
Your Name <hokandre@mhs.mdp.ac.id>
2018-09-10 21:22:58 +0700

    inheritence

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 "use strict"
+class Animal{
+    constructor(){
+        this.blood='merah'
+        this.mata='punya mata'
+        this.berkembangBiak='bisa'
+        this.SuperPower=new SuperPower()
+    }
+}
+class SuperPower{
+    use_laser_vision(){
+        return "Tembak pake Laser,,, ciuuu,,ciuuu"
+    }
+    be_invisible(){
+        return "tidak kelihatan! booom Auwwww"
+    }
+}
+class BerkakiDua extends Animal{
+    constructor(kaki,jenisDarah){
+        super()
+        this.kaki=kaki
+        this.isWarmBloode=jenisDarah
+    }
+}
+class BerkakiEmpat extends Animal{
+    constructor(kaki,jenisDarah){
+        super()
+        this.kaki=kaki
+        this.isWarmBloode=jenisDarah
+    }
+}
+var fox=new BerkakiEmpat(4,"panas")
+console.log(fox)
+console.log(fox.SuperPower.use_laser_vision())
+var ayam = new BerkakiDua(2,'dingin')
+console.log(ayam.SuperPower.use_laser_vision())
